b4ef59084e95645c6843dc64b17b08f18f9ea2b6
davidsamueljones <davidsamueljones@icloud.com>
2014-08-19 12:08:17 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -7,9 +7,16 @@ def DrawBlackSquare(size):
     move(size)
     turn(90)
     move(1)
-    size - 5
- 
+    
+def DrawWhiteSquare(size)
+  for i in range(0,4):
+  move(size)
+  turn(90)
+  move(1)
+
 size = 50
 for i in range(0,50):
   size = size - 1
   DrawBlackSquare(size)
+  
+DrawWhiteSquare(size)
