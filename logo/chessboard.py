39d550fce6ca307b300707d7f758e949c1798977
davidsamueljones <davidsamueljones@icloud.com>
2014-08-19 11:56:42 +0100

    Closing logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -3,12 +3,10 @@ from tealight.logo import (move,
                            color)
 
 def DrawBlackSquare(edges,size):
+  for i 
   if size < 50:
     move(size)
     turn(90)
-    stop = 0
-  if size == 50:
-    stop = 1
   
 turn(-90)
 DrawBlackSquare(12,20)
