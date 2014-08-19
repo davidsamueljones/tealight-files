f618862053d45d39d8864a263fd6c8ef651e98d7
davidsamueljones <davidsamueljones@icloud.com>
2014-08-19 11:58:59 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -4,9 +4,9 @@ from tealight.logo import (move,
 
 def DrawBlackSquare(edges,size):
   for i in range(0,20):
+    turn(90)
     move(size)
     turn(90)
     move(1)
-    turn(90)
   
 DrawBlackSquare(12,20)
