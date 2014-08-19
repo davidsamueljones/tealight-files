d37232951a58299c857671403e70debc8d9202f7
davidsamueljones <davidsamueljones@icloud.com>
2014-08-19 11:45:59 +0100

    Running logo/waterwheel.py

--- logo/waterwheel.py
+++ logo/waterwheel.py
@@ -1,17 +1,17 @@
 from tealight.logo import move, turn
 
 
-def triangle(side):
-  for i in range(0,3):
+def square(side):
+  for i in range(0,4):
     move(side)
-    turn(60)
+    turn(90)
 
 def waterwheel(edges, size):
   angle = 360 / edges
   decoration = size / 2
   for i in range(0, edges):
     move(size)
-    triangle(decoration)
+    square(decoration)
     turn(angle)
 
 turn(-90)
