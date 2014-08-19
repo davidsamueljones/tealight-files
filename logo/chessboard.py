e21833659818019afb694b7cb43e5f2f82fb7a9b
davidsamueljones <davidsamueljones@icloud.com>
2014-08-19 11:56:20 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -6,9 +6,9 @@ def DrawBlackSquare(edges,size):
   if size < 50:
     move(size)
     turn(90)
-    stop = false
+    stop = 0
   if size == 50:
-    stop = true
+    stop = 1
   
 turn(-90)
 DrawBlackSquare(12,20)
