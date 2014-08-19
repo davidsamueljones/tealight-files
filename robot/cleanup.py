48225162ea5824aaf2d6282c2731c65480b0f6da
davidsamueljones <davidsamueljones@icloud.com>
2014-08-19 15:28:22 +0100

    Running robot/cleanup.py

--- robot/cleanup.py
+++ robot/cleanup.py
@@ -6,4 +6,31 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-# Add your code here
+wallcount = 0
+
+def MoveSearch(thing):
+  global wallcount
+  wallcount = 0
+  distance = 1
+  i = 0
+  while i < distance:
+    if touch() == thing:
+      distance += 1
+    i += 1
+    if i < distance:
+      move()
+
+z = 0
+while z < 512:
+  z+=1
+  if touch() == 'fruit':
+    MoveSearch('fruit')
+  elif left_side() == 'fruit':
+    turn(-1)
+  elif right_side() == 'fruit':
+    turn(1)
+  elif look() == 'wall' and wallcount < 4:
+    turn(1)
+    wallcount += 1
+  elif look() == 'fruit':
+    MoveSearch(None)
