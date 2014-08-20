c7c23a4e062743c7b66a9028e24127abf152f7e8
davidsamueljones <davidsamueljones@icloud.com>
2014-08-20 12:29:01 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -1,6 +1,10 @@
 from random import random, randint
 
-BombArray = [[0 for x in range(10)] for x in range(10)] 
+NumberOfBombs = 10
+
+BombArray = [[0 for x in range(10)] for x in range(10)]
+
+for i in range(0,10)
 x = randint(0,9)
 y = randint(0,9)
 BombArray[x][y] = 1
