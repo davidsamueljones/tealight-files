8b79f75ef372f94423c97d07f9e36c1f000e14a5
davidsamueljones <davidsamueljones@icloud.com>
2014-08-20 09:47:57 +0100

    Running art/stars.py

--- art/stars.py
+++ art/stars.py
@@ -9,8 +9,8 @@ def star(x, y, c, vertical, horizontal, spines):
   angle = 0
   
   for i in range(0, spines):
-    x0 = x + (size * cos(angle))
-    y0 = y + (size * sin(angle))
+    x0 = x + (horizontal * cos(angle))
+    y0 = y + (vertical * sin(angle))
     
     line(x, y, x0, y0)
     
