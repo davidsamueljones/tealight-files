e5406e4c019d0d80ec10cce14c66701c9512b557
davidsamueljones <davidsamueljones@icloud.com>
2014-08-20 12:30:20 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -1,13 +1,17 @@
 from random import random, randint
 
 NumberOfBombs = 10
-
+BombsPlaced = 0
 BombArray = [[0 for x in range(10)] for x in range(10)]
 
-for i in range(0,10)
-x = randint(0,9)
-y = randint(0,9)
-BombArray[x][y] = 1
+while BombsPlaced < NumberOfBombs + 1
+  x = randint(0,9)
+  y = randint(0,9)
+  if BombArray[x][y] = 0
+    BombArray[x][y] = 1
+    BombsPlaced += 1
+  
+  
 
 print BombArray[x][y]
 print x
