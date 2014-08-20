1b1f8a4573c5ac2d931b570e87e97554ab54bfb5
davidsamueljones <davidsamueljones@icloud.com>
2014-08-20 09:25:08 +0100

    Running art/images.py

--- art/images.py
+++ art/images.py
@@ -1,7 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 x = 0
-y = 150
+y = 50
 
 width = 20
 height = 8
@@ -9,7 +9,7 @@ height = 8
 for i in range(0,width):
   for j in range(0,height):
     if (i - j) % 4 == 0:
-      image(x + j * 60, y + i * 30, "misc/YellowFlower.png")
+      image(x + j * 60, y + i * 60, "misc/YellowFlower.png")
     else:
-      image(x + j * 60, y + i * 30, "misc/Clover.png")
+      image(x + j * 60, y + i * 60, "misc/Clover.png")
      
