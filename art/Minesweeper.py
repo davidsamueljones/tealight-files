861490ea4df7d5638fc262f7291a5f5af8f09aeb
davidsamueljones <davidsamueljones@icloud.com>
2014-08-20 12:19:01 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -1,1 +1,11 @@
 Matrix = [[0 for x in xrange(5)] for x in xrange(5)] 
+import numpy
+>>> numpy.zeros((5, 5))
+array([[ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.]])
+>>> numpy.matrix([[1, 2],[3, 4]])
+matrix([[1, 2],
+        [3, 4]])
