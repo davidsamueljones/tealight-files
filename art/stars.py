8c8e2f3d9657244296ae1088edd22ef2020a1137
davidsamueljones <davidsamueljones@icloud.com>
2014-08-20 09:16:47 +0100

    Running art/stars.py

--- art/stars.py
+++ art/stars.py
@@ -10,12 +10,12 @@ def star(x, y, c, size, spines):
   
   for i in range(0, spines):
     x0 = x + (size * cos(angle))
-    y0 = y + (size * sin(angle))
+    y0 = y + (size * sin(angle) + 10)
     
     line(x, y, x0, y0)
     
     angle = angle + (2 * pi / spines)
 
-star(300, 300, "blue", 100, 23)
+star(300, 300, "blue", 100, 50)
 star(600, 400, "purple", 200, 100)
 star(450, 200, "orange", 125, 30)
