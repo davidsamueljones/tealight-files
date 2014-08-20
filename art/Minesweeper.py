19c48c4de00dee3af700eee12f7d9c2e9e084112
davidsamueljones <davidsamueljones@icloud.com>
2014-08-20 12:27:17 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -1,6 +1,8 @@
 from random import random, randint
 
-BombArray = [[0 for x in range(10)] for x in range(10)] 
-Matrix[0][0] = 1
+BombArray = [[0 for x in range(9)] for x in range(9)] 
+x = randint(0,9)
+y = randint(0,9)
+BombArray[0][0] = 1
 
 print Matrix[0][0]
