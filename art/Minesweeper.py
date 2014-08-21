f3749b9df794f973782e8e064a4b5ea00b464064
davidsamueljones <davidsamueljones@icloud.com>
2014-08-21 15:09:44 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -137,7 +137,7 @@ def FloodBoard(x,y):
   for (i,j) in [(x-1,y-1),(x-1,y), (x-1, y+1), (x,y-1), (x, y+1), (x+1,y-1),(x+1,y), (x+1, y+1)]:
   
     if (i >= 0 and i < WLimit and j >= 0 and j < HLimit):
-
+      n = 0
 NumberOfBombs = 1
 NumberUncovered = 0
 HLimit = 10
