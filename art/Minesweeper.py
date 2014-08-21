97050c64877daec23ecde395854037b45ea9a467
davidsamueljones <davidsamueljones@icloud.com>
2014-08-21 10:32:16 +0100

    Closing art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -3,16 +3,18 @@ from random import random, randint
 NumberOfBombs = 15
 HLimit = 10
 WLimit = 10
-BombsPlaced = 0
 
 BombArray = [[0 for x in range(HLimit)] for y in range(WLimit)]
 VisibleArray = [[0 for x in range(HLimit)] for y in range(WLimit)]
 
-while BombsPlaced < NumberOfBombs + 1:
-  x = randint(0,9)
-  y = randint(0,9)
-  if BombArray[x][y] == 0:
-    BombArray[x][y] = 1
-    BombsPlaced += 1
+def PlaceBombs(NumberOfBombs)
+BombsPlaced = 0
+  while BombsPlaced < NumberOfBombs + 1:
+    x = randint(0,9)
+    y = randint(0,9)
+    if BombArray[x][y] == 0:
+      BombArray[x][y] = 1
+      BombsPlaced += 1
   
+def DrawGrid()
   
