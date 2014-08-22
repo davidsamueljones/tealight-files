1311ddf6c7f1cf1197d66b4ec9f0264b4ea85af8
davidsamueljones <davidsamueljones@icloud.com>
2014-08-22 08:55:09 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -1,5 +1,5 @@
 from random import random, randint
-from tealight.art import (color, line, spot, circle, box, image, text, background)
+from tealight.art import (color, line, spot, circle, box, font, image, text, background)
 from tealight.art import (screen_width, screen_height)
 from tealight.utils import (sleep)
 
