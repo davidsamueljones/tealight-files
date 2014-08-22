aab37b6d1b4a9cc82defc2a33d8eecba5be82cd8
davidsamueljones <davidsamueljones@icloud.com>
2014-08-22 09:43:03 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -170,7 +170,7 @@ def FloodBoard(x,y):
           FloodBoard(i,j)
       
 NumberOfBombs = 25
-HLimit = 20
+HLimit = 10
 WLimit = HLimit
 SquareSize = 500/HLimit
 StartingX = screen_width /2 - SquareSize * WLimit/2
